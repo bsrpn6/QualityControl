eef12362a85d3238efb1b5fd214f8c555deb1b2d
Brandon Ray <bsrpn6@gmail.com>
2024-02-23 13:56:33 -0600

    Upgrading AGP dependency from 8.1.1 to 8.2.2 and Gradle plugin to 8.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.0.0" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
@@ -9,7 +9,7 @@ plugins {
 }
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.2.2")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.gms:google-services:4.3.15")
